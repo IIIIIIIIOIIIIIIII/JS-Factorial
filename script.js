cbb75c2796b411081392fc6138f4ddb9ae40c96f
Yuri <44642611+gryymenael@users.noreply.github.com>
2019-06-24 16:48:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+﻿function createAnswerField(){
+	const parentBlock = document.body;
+	const answer = document.createElement('p');
+	answer.id = 'answer';
+	parentBlock.appendChild(answer);	
+}
+
+function findFactorial(){
+	
+	const inputField = document.getElementById('factorial');
+	let n = Number(document.getElementById('factorial').value);	
+	let result = 1;
+	let i = 2;
+	
+	while(i <= n){		
+		
+		result *= i;		
+		i++;		
+	}
+	
+	document.getElementById('answer').textContent = result;
+	
+}
+
+createAnswerField();
+document.getElementById('button').addEventListener('click', findFactorial);
